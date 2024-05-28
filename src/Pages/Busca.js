ccafdccc50cad078a5ea042513dd39e9a2545383
mepaula <melinad.pontes@gmail.com>
2024-05-28 15:29:52 -0300

    quase pronto o projeto

--- src/Pages/Busca.js
+++ src/Pages/Busca.js
@@ -1,72 +1,212 @@
 import { View, Text, TextInput, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function Busca() {
     const [usuarios, setUsuarios ] = useState( [] );
     const [error, setError ] = useState(false);
     const [busca, setBusca] = useState(false);
     const [filtro, setFiltro ] = useState(false);
+    const[edicao, setEdicao ] = useState(false);
+    const [userId, setUserId ] = useState(0);
+    const [UserNome, setNome ] = useState();
+    const [userEmail, setEmail ] = useState();
+    const [userSenha, setSenha ] = useState();
+    const [ deleteResposta, setResposta] = useState(false);
 
-    async function getUsuarios()
-    {
-        await fetch('https://fakestoreapi.com/users', {
+    async function getUsuarios(){
+        await fetch('http://10.139.75.26:5251/api/Users/GetAllUsers',{
             method: 'GET',
             headers: {
-              'content-type': 'application/json'
+                'content-type':'application/json'
             }
-          })
-            .then( res => ( res.ok == true ) ? res.json() : false )
-            .then( json => setUsuarios( json ) )
-            .catch( err => setError( true ) )
+        })
+        .then(res=> res.json())
+        .then(json=> setUsuarios(json))
+        .catch(err=> setError(true))
     }
+
+    async function getUsuario()
+    {
+        await fetch('http://10.139.75.26:5251/api/Users/GetAllUsers'+ userId, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            },
+            boby: JSON.stringify({
+                userId: userId,
+                userEmail: userEmail,
+                userPassword: userSenha,
+                userName: UserNome
+            })
+        })
+        .then((responde) => responde.json())
+        .catch( err => console.log( err ) );
+        getUsuario();
+        setEdicao(false);
+    }
+
+    async function editUser(){
+        await fetch('http://10.139.75.26:5251/api/Users/GetAllUsers' + userId, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json; charset-UTF-8',
+            },
+            body: JSON.stringify({
+                userId: userId,
+                userEmail: userEmail,
+                userPassword: userSenha,
+                userName: UserNome
+            })
+        })
+            .then((reponde) => reponde.json())
+            .catch(err => console.log(err));
+        getUsuario();
+        setEdicao(false);
+    }
+
+    function showAlert(id, userName) {
+        Alert.alert(
+            '',
+            'Deseja realmente excluir esse usuário ?',
+            [
+                { text: 'Sim', onPress: () => deleteUsuario(id, userName)},
+                { text: 'Não', onPress: () =>('')},
+            ],
+            { cancelable: false}
+        );
+    }
+
+    async function deleteUsuario(id, userName){
+        await fetch('http://10.139.75.26:5251/api/Users/DeleteUser/' + id, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json; charset-UTF-8',
+            },
+        })
+        .then( res => res.json())
+        .then(json => setResposta(json))
+        .catch( err => setError(true))
+
+        if(deleteResposta == true)
+        {
+            Alert.alert(
+                '',
+                'Usuario' + userName + 'excluindo com sucesso',
+                [
+                    { text: '', onPress: () => ('')},
+                    { text: 'Ok', onPress: () =>('')},
+                ],
+                { cancelable: false}
+            );
+            getUsuarios();
+        }
+
+        else{
+            Alert.alert(
+                '',
+                'Usuario' + userName + 'não foi excluido.',
+                [
+                    { text: '', onPress: () =>('')},
+                    { text: 'Ok', onPress: () =>('')},
+                ],
+                { cancelable: false}
+            );
+            getUsuarios();
+        }
+    };
 
     useEffect( () => {
         getUsuarios();
-    }, [] );
+    }, []);
 
-    useEffect( () => {
-        setFiltro( usuarios.filter( (item) => item.name.firstname == busca )[0] );
-    }, [busca] );
+    useFocusEffect(
+        React.useCallback(() => {
+            getUsuarios();
+        }, [])
+    );
 
-    return (
-        <View style={css.container}>
-            <View style={css.searchBox}>
-                <TextInput
-                    style={css.search}
-                    placeholder="Buscar usuarios"
-                    placeholderTextColor="white"
-                    TextInput={busca}
-                    onChangeText={(digitado) => setBusca( digitado ) }
+    return(
+        <View style={style.container}>
+            {edicao == false ?
+            <FlatList 
+            style={style.flat}
+            data={usuarios}
+            keyExtractor={(item) => item.UserId}
+            renderItem={({ item }) => (
+                <Text style={style.text}>
+                    {item.userName}
+                    <TouchableOpacity style={style.btnEdit} onPress={() => { setEdicao(true); getUsuario(item.UserId)}}>
+                        <Text style={style.btntexto}>EDITAR</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={style.btnDelete} onPress={() => showAlert(item.UserId, item.userName)}>
+                        <Text style={style.btntexto}>EXCLUIR</Text>
+                    </TouchableOpacity>
+                </Text>
+            )}
+            />
+            :
+            <View style={style.editar}>
+                <TextInput 
+                inputMode="text"
+                style={style.input}
+                value={UserNome}
+                onChangeText={(digitado) => setNome(digitado)}
+                placeholder="white"
                 />
+                <TextInput 
+                inputMode="email"
+                style={style.input}
+                value={userEmail}
+                onChangeText={(digitado) => setEmail(digitado)}
+                placeholder="white"
+                />
+                <TextInput 
+                inputMode="text"
+                secureTextEntry={true}
+                style={style.input}
+                value={userSenha}
+                onChangeText={(digitado) => setSenha(digitado)}
+                placeholder="white"
+                />
+                <TouchableOpacity style={style.btnCreate} onPress={() => editUser()}>
+                    <Text style={style.btnLoginText}>SALVAR</Text>
+                </TouchableOpacity>
             </View>
-            { filtro && <Text style={css.text}>{filtro.name.firstname} {filtro.name.lastname}</Text> }
-            { ( !filtro && busca ) && <ActivityIndicator size="large" color="white" /> }
+
+            }
         </View>
-    )
+    );
 }
-const css = StyleSheet.create({
+const style = StyleSheet.create({
     container: {
         flexGrow: 1,
         width: "100%",
         alignItems: "center",
-        backgroundColor: "#191919",
+        backgroundColor: "black",
     },
     text: {
+        borderBlockColor: "white",
         color: "white"
     },
-    searchBox: {
+    btntexto: {
         width: "100%",
         height: 100,
         justifyContent: "flex-end",
         alignItems: "center",
     },
-    search: {
-        width: "96%",
-        height: 60,
-        borderWidth: 1,
-        borderColor: "white",
-        borderRadius: 8,
-        padding: 10,
+    btnEdit: {
+        width: 250, // Largura do botão
+        height: 20, // Altura do botão
+        backgroundColor: "red",
+        color: "white"
+    },
+    btnDelete: {
+        width: 250, // Largura do botão
+        height: 20, // Altura do botão
+        backgroundColor: "red",
         color: "white"
     }
 })
--- src/Pages/Inserir.js
+++ src/Pages/Inserir.js
@@ -1,22 +1,178 @@
-import { View, Text, StyleSheet} from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
+
 
 export default function Inserir() {
-  return (
-    <View style={css.container}>
-      <Text style={css.text}>Inserir</Text>
-    </View>
-  )
+
+    const[nome, setNome ] = useState("");
+    const[sobrenome, setSobreNome ] = useState("");
+    const[ usuario, setUsuario ] = useState("");
+    const[email, setEmail ] = useState("");
+    const[telefone, setTelefone ] = useState("");
+    const[rua, setRua ] = useState("");
+    const[cidade, setCidade ] = useState("");
+    const[cpf, setCpf ] = useState("");
+    const[senha, setSenha ] = useState("");
+    const[numero, setNumero ] = useState("");
+    const[erro, setErro ] = useState(false);
+    const[sucesso, setSucesso ] = useState(false);
+
+    async function Cadastro()
+    {
+        await fetch('https://fakestoreapi.com/auth/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email:email,
+                username:usuario,
+                password:senha,
+                name:{
+                    firstname: nome,
+                    lastname: sobrenome,
+                },
+                address:{
+                    city: cidade,
+                    street:rua,
+                    number: numero,
+                    zipcode: cpf,
+                    geolocation:{
+                        lat:'-37.3159',
+                        long:'81.1496'
+                    }
+                },
+                phone: telefone
+            })
+        })
+        .then(res => (res.ok == true) ? res.json() :false )
+        .then(json => console.log( json ) )
+        .catch(err => setErro( true ) )
+    }
+
+    return(
+        <ScrollView contentContainerStyle={style.container}>
+            {sucesso ?
+                <Text style={style.text}>Obrigado por se cadastrar. Seu cadastro foi realizado com sucesso!</Text>
+                :
+                <>
+
+                <TextInput 
+                    style={style.input}
+                    placeholder="Nome"
+                    placeholderTextColor="black"
+                    TextInput={nome}
+                    onChangeText={(digitado) => setNome( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="SobreNome"
+                    placeholderTextColor="black"
+                    TextInput={sobrenome}
+                    onChangeText={(digitado) => setSobreNome( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="Usuario"
+                    placeholderTextColor="black"
+                    TextInput={usuario}
+                    onChangeText={(digitado) => setUsuario( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="Senha"
+                    placeholderTextColor="black"
+                    TextInput={senha}
+                    onChangeText={(digitado) => setSenha( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="Númro"
+                    placeholderTextColor="black"
+                    TextInput={numero}
+                    onChangeText={(digitado) => setNumero( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="Email"
+                    placeholderTextColor="black"
+                    TextInput={email}
+                    onChangeText={(digitado) => setEmail( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="Rua"
+                    placeholderTextColor="black"
+                    TextInput={rua}
+                    onChangeText={(digitado) => setRua( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="Telefone"
+                    keyboardType="numeric"
+                    placeholderTextColor="black"
+                    TextInput={telefone}
+                    onChangeText={(digitado) => setTelefone( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="Cidade"
+                    placeholderTextColor="black"
+                    TextInput={cidade}
+                    onChangeText={(digitado) => setCidade( digitado ) }
+                />
+                <TextInput 
+                    style={style.input}
+                    placeholder="CPF"
+                    placeholderTextColor="black"
+                    TextInput={cpf}
+                    onChangeText={(digitado) => setCpf( digitado ) }
+                />
+                <TouchableOpacity style={style.btnCadastrar}>
+                    <Text style={style.CadastrarText}>Cadastrar</Text>
+                </TouchableOpacity>
+                {erro && <Text>Revise cuidadosamente os campos!</Text>}
+             </>
+            }
+        </ScrollView>
+    )
 }
-const css = StyleSheet.create({
-  container: {
-    backgroundColor: "#191919",
-    flexGrow: 1,
-    color: "white",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  text: {
-    color: "white"
-  }
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        flexGrow: 1,
+        color: "white",
+        justifyContent: "center",
+        alignItens: "center",
+        padding: 50,
+        paddingBottom: 50
+    },
+    text: {
+        color: "black"
+    },
+    input: {
+        fontSize: 15,
+        alignItems: "center",
+        color: 'black',
+        borderWidth: 1,
+        height: 60,
+        padding: 14,
+        borderRadius: 25
+    },
+    btnCadastrar: {
+        alignItems: 'center'
+    },
+    CadastrarText: {
+        width: 220, // Largura do botão
+        height: 45, // Altura do botão
+        marginBottom: 15, // Espaçamento inferior
+        borderRadius: 15, // Borda arredondada
+        justifyContent: 'center', // Centraliza o texto dentro do botão
+        alignItems: 'center', // Centraliza o texto dentro do botão
+        backgroundColor: '#f6b0f7', // Cor de fundo do botão
+        textAlign: 'center',
+        fontSize: 26,
+        color: 'black',
+    }
 })
